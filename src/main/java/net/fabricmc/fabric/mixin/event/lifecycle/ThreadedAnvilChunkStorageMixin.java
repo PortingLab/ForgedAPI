1cede2f2d29ffe18ae8f2f2f1d66b2cb5cbba35b
TexTrue <3140846162@qq.com>
2023-06-18 17:00:13 +0800

    f-k

--- src/main/java/net/fabricmc/fabric/mixin/event/lifecycle/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/net/fabricmc/fabric/mixin/event/lifecycle/ThreadedAnvilChunkStorageMixin.java
@@ -59,7 +59,6 @@ public abstract class ThreadedAnvilChunkStorageMixin {
 	@Shadow @Final private Queue<Runnable> unloadTaskQueue;
 	@Shadow @Final private Long2LongMap chunkToNextSaveTimeMs;
 	@Shadow @Final private MessageListener<ChunkTaskPrioritySystem.Task<Runnable>> mainExecutor;
-	@Shadow @Final protected abstract void addEntitiesFromNbt(ServerWorld world, List<NbtCompound> nbt);
 
 	// Chunk (Un)Load events, An explanation:
 	// Must of this code is wrapped inside of futures and consumers, so it's generally a mess.
@@ -67,46 +66,7 @@ public abstract class ThreadedAnvilChunkStorageMixin {
 	/**
 	 * Injection is inside of tryUnloadChunk.
 	 * We inject just after "setLoadedToWorld" is made false, since here the WorldChunk is guaranteed to be unloaded.
-	 *
-	 * @author Kasualix, TexTrue
-	 * @reason insert event invoker
 	 */
-	@Overwrite
-	private void tryUnloadChunk(long pos, ChunkHolder holder) {
-		CompletableFuture<Chunk> completablefuture = holder.getSavingFuture();
-		Consumer var10001 = (chunk) -> {
-			CompletableFuture<Chunk> completablefuture1 = holder.getSavingFuture();
-			if (completablefuture1 != completablefuture) {
-				this.tryUnloadChunk(pos, holder);
-			} else if (this.chunksToUnload.remove(pos, holder) && chunk != null) {
-				if (chunk instanceof WorldChunk) {
-					((WorldChunk)chunk).setLoadedToWorld(false);
-					ServerChunkEvents.CHUNK_UNLOAD.invoker().onChunkUnload(this.world, (WorldChunk) chunk);
-					MinecraftForge.EVENT_BUS.post(new ChunkEvent.Unload((Chunk) chunk));
-				}
-
-				this.save((Chunk) chunk);
-				if (this.loadedChunks.remove(pos) && chunk instanceof WorldChunk) {
-					WorldChunk levelchunk = (WorldChunk)chunk;
-					this.world.unloadEntities(levelchunk);
-				}
-
-				((ExtendedServerLightingProvider)this.lightingProvider).forgedapi$updateChunkStatus(((Chunk) chunk).getPos());
-				this.lightingProvider.tick();
-				this.worldGenerationProgressListener.setChunkStatus(((Chunk) chunk).getPos(), (ChunkStatus)null);
-				this.chunkToNextSaveTimeMs.remove(((Chunk) chunk).getPos().toLong());
-			}
-
-		};
-		Queue var10002 = this.unloadTaskQueue;
-		Objects.requireNonNull(var10002);
-		completablefuture.thenAcceptAsync(var10001, var10002::add).whenComplete((void_, throwable) -> {
-			if (throwable != null) {
-				LOGGER.error("Failed to save chunk {}", holder.getPos(), throwable);
-			}
-
-		});
-	}
 	/*
 	@Inject(method = "method_18843", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/WorldChunk;setLoadedToWorld(Z)V", shift = At.Shift.AFTER))
 	private void onChunkUnload(ChunkHolder chunkHolder, CompletableFuture<Chunk> chunkFuture, long pos, Chunk chunk, CallbackInfo ci) {
@@ -121,55 +81,7 @@ public abstract class ThreadedAnvilChunkStorageMixin {
 	 *
 	 * <ul><li>the chunk being loaded MUST be a WorldChunk.
 	 * <li>everything within the chunk has been loaded into the world. Entities, BlockEntities, etc.</ul>
-	 *
-	 * @author Kasualix, TexTrue
-	 * @reason insert event invoker
 	 */
-	@Overwrite
-	private CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> convertToFullChunk(ChunkHolder chunkHolder) {
-		CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> completablefuture = chunkHolder.getFutureFor(ChunkStatus.FULL.getPrevious());
-		return completablefuture.thenApplyAsync((either) -> {
-			ChunkStatus chunkstatus = ChunkHolder.getTargetStatusForLevel(chunkHolder.getLevel());
-			return !chunkstatus.isAtLeast(ChunkStatus.FULL) ? ChunkHolder.UNLOADED_CHUNK : either.mapLeft((protoChunk) -> {
-				ChunkPos chunkpos = chunkHolder.getPos();
-				ProtoChunk protochunk = (ProtoChunk)protoChunk;
-				WorldChunk levelchunk;
-				if (protochunk instanceof ReadOnlyChunk) {
-					levelchunk = ((ReadOnlyChunk)protochunk).getWrappedChunk();
-				} else {
-					levelchunk = new WorldChunk(this.world, protochunk, (chunk) -> {
-						addEntitiesFromNbt(this.world, protochunk.getEntities());
-					});
-					chunkHolder.setCompletedChunk(new ReadOnlyChunk(levelchunk, false));
-				}
-
-				levelchunk.setLevelTypeProvider(() -> {
-					return ChunkHolder.getLevelType(chunkHolder.getLevel());
-				});
-				levelchunk.loadEntities();
-				if (this.loadedChunks.add(chunkpos.toLong())) {
-					levelchunk.setLoadedToWorld(true);
-
-					try {
-						((ExtendedChunkHolder) chunkHolder).setCurrentlyLoadingWorldChunk(levelchunk);
-						levelchunk.updateAllBlockEntities();
-						levelchunk.addChunkTickSchedulers(this.world);
-						ServerChunkEvents.CHUNK_LOAD.invoker().onChunkLoad(this.world, levelchunk);
-						MinecraftForge.EVENT_BUS.post(new ChunkEvent.Load(levelchunk));
-					} finally {
-						((ExtendedChunkHolder) chunkHolder).setCurrentlyLoadingWorldChunk(null);
-					}
-				}
-
-				return levelchunk;
-			});
-		}, (task) -> {
-			MessageListener var10000 = this.mainExecutor;
-			long var10002 = chunkHolder.getPos().toLong();
-			Objects.requireNonNull(chunkHolder);
-			var10000.send(ChunkTaskPrioritySystem.createMessage(task, var10002, chunkHolder::getLevel));
-		});
-	}
 	/*
 	@Inject(method = "method_17227", at = @At("TAIL"))
 	private void onChunkLoad(ChunkHolder chunkHolder, Chunk protoChunk, CallbackInfoReturnable<Chunk> callbackInfoReturnable) {
